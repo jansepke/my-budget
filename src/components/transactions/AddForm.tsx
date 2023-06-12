97fb9392dd2480358bf0242ff836ae2889fdf170
Jan Sepke <625043+jansepke@users.noreply.github.com>
2023-06-12 15:51:00 +0200

    form design

--- src/components/transactions/AddForm.tsx
+++ src/components/transactions/AddForm.tsx
@@ -1,4 +1,11 @@
+import EuroIcon from "@mui/icons-material/Euro";
+import EventIcon from "@mui/icons-material/Event";
+import TitleIcon from "@mui/icons-material/Title";
+import AddIcon from "@mui/icons-material/Add";
+import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import FormControl from "@mui/material/FormControl";
+import InputAdornment from "@mui/material/InputAdornment";
 import TextField from "@mui/material/TextField";
 import { DateField } from "@mui/x-date-pickers";
 
@@ -8,14 +15,52 @@ export const AddForm = () => {
   };
 
   return (
-    <form onSubmit={addTransaction}>
-      <DateField label="Date" format="DD-MM-YYYY" />
-      <TextField label="Description" variant="outlined" />
-      <TextField label="Amount" variant="outlined" type="number" />
-      <br />
-      <Button type="submit" variant="contained">
+    <Box
+      component="form"
+      sx={{
+        "& .MuiTextField-root": { m: 1 },
+      }}
+      noValidate
+      autoComplete="off"
+      onSubmit={addTransaction}
+    >
+      <FormControl component="fieldset" sx={{ flexDirection: "row" }}>
+        <DateField
+          label="Date"
+          format="DD-MM-YYYY"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <EventIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+        <TextField
+          label="Description"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <TitleIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+        <TextField
+          label="Amount"
+          inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <EuroIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </FormControl>
+      <Button type="submit" variant="contained" startIcon={<AddIcon />}>
         Add
       </Button>
-    </form>
+    </Box>
   );
 };
