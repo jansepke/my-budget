866d906300d04de1265b563988a0d181014bbf35
Jan Sepke <625043+jansepke@users.noreply.github.com>
2023-06-15 14:27:41 +0200

    extract current month tile

---
+++ src/components/dashboard/CurrentMonthTile.tsx
@@ -0,0 +1,23 @@
+import { TransactionList } from "@/components/transactions/TransactionList";
+import { TransactionStats } from "@/components/transactions/TransactionStats";
+import { Transaction } from "@/domain";
+import NorthEastIcon from "@mui/icons-material/NorthEast";
+import { Link as MuiLink } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import Link from "next/link";
+
+interface CurrentMonthTileProps {
+  transactions: Transaction[];
+}
+
+export const CurrentMonthTile: React.FC<CurrentMonthTileProps> = ({ transactions }) => (
+  <>
+    <Typography variant="h5">
+      <MuiLink component={Link} href="/transactions/current/current" color="inherit" underline="hover">
+        Current Month <NorthEastIcon />
+      </MuiLink>
+    </Typography>
+    <TransactionStats transactions={transactions} />
+    <TransactionList transactions={transactions.slice(-5)} />
+  </>
+);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,12 @@
 import { getTransactionsForMonth } from "@/backend/transactions";
-import ProtectedPage from "@/components/shared/ProtectedPage";
 import { AddTransactionButton } from "@/components/dashboard/AddTransactionButton";
-import { TransactionList } from "@/components/transactions/TransactionList";
-import { TransactionStats } from "@/components/transactions/TransactionStats";
+import { CurrentMonthTile } from "@/components/dashboard/CurrentMonthTile";
+import ProtectedPage from "@/components/shared/ProtectedPage";
 import { Transaction } from "@/domain";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
-import NorthEastIcon from "@mui/icons-material/NorthEast";
-import { Link as MuiLink } from "@mui/material";
 import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
 import { GetServerSideProps } from "next";
 import { getServerSession } from "next-auth";
-import Link from "next/link";
 
 interface IndexPageProps {
   transactions: Transaction[];
@@ -20,13 +15,7 @@ interface IndexPageProps {
 const IndexPage: React.FC<IndexPageProps> = ({ transactions }) => (
   <ProtectedPage headline="My Budget">
     <Container maxWidth="md" sx={{ marginTop: 3 }}>
-      <Typography variant="h5">
-        <MuiLink component={Link} href="/transactions/current/current" color="inherit" underline="hover">
-          Current Month <NorthEastIcon />
-        </MuiLink>
-      </Typography>
-      <TransactionStats transactions={transactions} />
-      <TransactionList transactions={transactions.slice(-5)} />
+      <CurrentMonthTile transactions={transactions} />
 
       <AddTransactionButton />
     </Container>
