e67c20c4cb2665429b0f18a41ff221592f99cb53
Jan Sepke <625043+jansepke@users.noreply.github.com>
2023-07-24 22:50:27 +0200

    first sub category grouping in report

--- src/components/reports/CategoryReport.tsx
+++ src/components/reports/CategoryReport.tsx
@@ -1,5 +1,6 @@
 import { CategoryStats } from "@/domain";
 import { currencyColor, formatCurrency } from "@/utils";
+import IconButton from "@mui/material/IconButton";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -7,20 +8,47 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import React from "react";
+import React, { useState } from "react";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+
+interface ExpandableTableRowProps extends React.PropsWithChildren {
+  expandComponent: React.ReactNode;
+}
+
+const ExpandableTableRow: React.FC<ExpandableTableRowProps> = ({ children, expandComponent }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <>
+      <TableRow hover>
+        <TableCell>
+          <IconButton onClick={() => setIsExpanded(!isExpanded)}>
+            {isExpanded ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        {children}
+      </TableRow>
+      {isExpanded && expandComponent}
+    </>
+  );
+};
 
 interface CategoryReportProps {
   categoryStats: CategoryStats[];
 }
 
 export const CategoryReport: React.FC<CategoryReportProps> = ({ categoryStats }) => {
+  const topCategoryStats = calculateTopCategoryStats(categoryStats);
+
   return (
     <TableContainer component={Paper}>
       <Table>
         <TableHead>
           <TableRow>
+            <TableCell></TableCell>
             <TableCell width={75}>value</TableCell>
-            <TableCell>label</TableCell>
+            <TableCell width="100%">label</TableCell>
             <TableCell width={90} align="right">
               yearAverage
             </TableCell>
@@ -30,22 +58,56 @@ export const CategoryReport: React.FC<CategoryReportProps> = ({ categoryStats })
           </TableRow>
         </TableHead>
         <TableBody>
-          {categoryStats.map((stat, idx) => (
-            <TableRow key={idx} hover>
+          {topCategoryStats.map((stat, idx) => (
+            <ExpandableTableRow
+              key={idx}
+              expandComponent={
+                <>
+                  {categoryStats
+                    .filter((subStat) => subStat.value.length > 1 && subStat.value.startsWith(stat.value))
+                    .map((subStat, idx) => (
+                      <TableRow key={idx}>
+                        <TableCell />
+                        <TableCell>{subStat.value}</TableCell>
+                        <TableCell width="100%">{subStat.label}</TableCell>
+                        <TableCell align="right" sx={{ color: currencyColor(subStat.yearAverage) }}>
+                          {formatCurrency(subStat.yearAverage)}
+                        </TableCell>
+                        <TableCell align="right" sx={{ color: currencyColor(subStat.currentSum) }}>
+                          {formatCurrency(subStat.currentSum)}
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                </>
+              }
+            >
               <TableCell>{stat.value}</TableCell>
-              <TableCell sx={{ width: "100%", display: "flex", justifyContent: "space-between" }}>
-                {stat.label}
-              </TableCell>
+              <TableCell width="100%">{stat.label}</TableCell>
               <TableCell align="right" sx={{ color: currencyColor(stat.yearAverage) }}>
                 {formatCurrency(stat.yearAverage)}
               </TableCell>
               <TableCell align="right" sx={{ color: currencyColor(stat.currentSum) }}>
                 {formatCurrency(stat.currentSum)}
               </TableCell>
-            </TableRow>
+            </ExpandableTableRow>
           ))}
         </TableBody>
       </Table>
     </TableContainer>
   );
 };
+
+function calculateTopCategoryStats(categoryStats: CategoryStats[]) {
+  return categoryStats.reduce((all, stat) => {
+    if (stat.value.length < 2) {
+      return [...all, { ...stat }];
+    }
+
+    const group = all.find((g) => g.value === stat.value[0])!;
+
+    group.yearAverage += stat.yearAverage;
+    group.currentSum += stat.currentSum;
+
+    return all;
+  }, [] as CategoryStats[]);
+}
