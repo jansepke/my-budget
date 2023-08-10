d1465be631615a75b5c6f5c3fddf2ab751e411db
Jan Sepke <625043+jansepke@users.noreply.github.com>
2023-08-10 09:25:23 +0200

    add another test

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { sum } from "./utils";
+import { formatCurrency, sum } from "./utils";
 
 describe("sum", () => {
   it("sums transactions", () => {
@@ -8,5 +8,14 @@ describe("sum", () => {
     const result = sum(transactions);
 
     expect(result).toBe(3.2);
+  });
+});
+
+describe("formatCurrency", () => {
+  it("formats in EUR", () => {
+    const result = formatCurrency(123);
+
+    // https://github.com/testing-library/jest-dom/issues/376#issuecomment-885012111
+    expect(result.replace(/\u00a0/g, " ")).toBe("123,00 €");
   });
 });
