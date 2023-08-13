3c3469287184c1b13790857d9c629bde75f6d7f5
Jan Sepke <625043+jansepke@users.noreply.github.com>
2023-08-13 07:40:00 +0200

    add filtered sum

--- src/pages/transactions/[year]/[month].tsx
+++ src/pages/transactions/[year]/[month].tsx
@@ -8,9 +8,10 @@ import { TransactionList } from "@/components/transactions/TransactionList";
 import { TransactionStats } from "@/components/transactions/TransactionStats";
 import { Category, Transaction } from "@/domain";
 import { getSession } from "@/pages/api/auth/[...nextauth]";
-import { filterByMonth, filterForMainAccount } from "@/utils";
+import { filterByMonth, filterForMainAccount, formatCurrency, sum } from "@/utils";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
 import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
 
@@ -42,8 +43,11 @@ const TransactionsPage: React.FC<TransactionsPageProps> = (props) => {
       <Container maxWidth="md" sx={{ marginTop: 3 }}>
         <Toolbar year={props.year} month={props.month} />
         <TransactionStats accountId={1} transactions={props.transactions} showFixedSum />
-        <Box display="flex" justifyContent="center" alignItems="center" m={2}>
+        <Box display="flex" justifyContent="center" alignItems="center" gap={2} m={2}>
           <CategorySelector value={filterCategory} onChange={handleFilterCategory} categories={categories} />
+          {filterCategory != null && (
+            <Typography color="text.secondary">{formatCurrency(sum(filteredTransactions))} (filtered)</Typography>
+          )}
         </Box>
         <Box mb={10}>
           <TransactionList accountId={1} transactions={filteredTransactions} />
