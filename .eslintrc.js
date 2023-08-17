6790b156e56fc99b760f9bff024b8d25285891f4
Jan Sepke <625043+jansepke@users.noreply.github.com>
2023-08-17 06:27:51 +0200

    eslint type checked rules only on ts files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,39 @@
 module.exports = {
   root: true,
-  ignorePatterns: [".eslintrc.js"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic",
-    "plugin:@typescript-eslint/stylistic-type-checked",
     "next/core-web-vitals",
     "prettier",
   ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-    tsconfigRootDir: __dirname,
-  },
-  rules: {
-    "@typescript-eslint/no-misused-promises": [
-      "error",
-      {
-        checksVoidReturn: {
-          attributes: false,
-        },
-      },
-    ],
-  },
   overrides: [
     {
       files: ["**/*.js?(x)"],
       rules: {
         "@typescript-eslint/no-var-requires": "off",
+      },
+    },
+    {
+      files: ["**/*.ts?(x)"],
+      extends: [
+        "plugin:@typescript-eslint/recommended-type-checked",
+        "plugin:@typescript-eslint/stylistic-type-checked",
+      ],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+      },
+      rules: {
+        "@typescript-eslint/no-misused-promises": [
+          "error",
+          {
+            checksVoidReturn: {
+              attributes: false,
+            },
+          },
+        ],
       },
     },
   ],
