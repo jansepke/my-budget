8c701c6f994bd00a20506e84cf93d6c934fbe807
Jan Sepke <625043+jansepke@users.noreply.github.com>
2023-10-21 08:17:14 +0200

    do not apply headers locally

--- next.config.js
+++ next.config.js
@@ -1,33 +1,37 @@
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = (phase, { defaultConfig }) => ({
   reactStrictMode: true,
   async headers() {
-    return [
-      {
-        source: "/(.*?)",
-        headers: [
+    return phase === PHASE_DEVELOPMENT_SERVER
+      ? []
+      : [
           {
-            key: "Strict-Transport-Security",
-            value: "max-age=63072000; includeSubDomains; preload",
+            source: "/(.*?)",
+            headers: [
+              {
+                key: "Strict-Transport-Security",
+                value: "max-age=63072000; includeSubDomains; preload",
+              },
+              {
+                key: "X-Frame-Options",
+                value: "SAMEORIGIN",
+              },
+              {
+                key: "X-Content-Type-Options",
+                value: "nosniff",
+              },
+              {
+                key: "Referrer-Policy",
+                value: "strict-origin-when-cross-origin",
+              },
+              {
+                key: "Content-Security-Policy",
+                value: "default-src 'self'; style-src 'self' 'unsafe-inline';",
+              },
+            ],
           },
-          {
-            key: "X-Frame-Options",
-            value: "SAMEORIGIN",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin",
-          },
-          {
-            key: "Content-Security-Policy",
-            value: "default-src 'self'; style-src 'self' 'unsafe-inline';",
-          },
-        ],
-      },
-    ];
+        ];
   },
-};
+});
