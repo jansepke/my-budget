c259b607ae413435a118246df23c92201f0b9c71
Jan Sepke <625043+jansepke@users.noreply.github.com>
2024-02-20 06:58:44 +0100

    fix initial icon flickering

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,11 +2,28 @@ import { roboto } from "@/next/theme";
 import { DocumentHeadTags, DocumentHeadTagsProps, documentGetInitialProps } from "@mui/material-nextjs/v14-pagesRouter";
 import { DocumentProps, Head, Html, Main, NextScript } from "next/document";
 
+// Workaround for initial icon flickering
+const css = `
+body {
+  animation: fadeInAnimation ease 0.01s;
+}
+
+@keyframes fadeInAnimation {
+  0% {
+      opacity: 0;
+  }
+  100% {
+      opacity: 1;
+  }
+}
+`;
+
 export default function MyDocument(props: DocumentProps & DocumentHeadTagsProps) {
   return (
     <Html lang="en" className={roboto.className}>
       <Head>
         <DocumentHeadTags {...props} />
+        <style>{css}</style>
       </Head>
       <body>
         <Main />
