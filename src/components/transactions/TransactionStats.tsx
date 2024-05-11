2d1666ae21d4da7b4290e51ab728093e5546273d
Jan Sepke <625043+jansepke@users.noreply.github.com>
2024-05-11 07:30:07 +0200

    add detail tooltip

--- src/components/transactions/TransactionStats.tsx
+++ src/components/transactions/TransactionStats.tsx
@@ -1,6 +1,7 @@
 import { Transaction } from "@/domain";
 import { FIXED_GROUP, INCOME_CATEGORY, filterVariable, formatCurrency, sum } from "@/utils";
 import Box from "@mui/material/Box";
+import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
 
 interface TransactionStatsProps {
@@ -17,7 +18,18 @@ export const TransactionStats: React.FC<TransactionStatsProps> = ({ accountId, t
   return (
     <Box display="flex" justifyContent="space-between" m={2}>
       <Typography color="text.secondary">{formatCurrency(incomeSum)}</Typography>
-      <Typography color="text.secondary">{formatCurrency(fixedSum + variableSum)}</Typography>
+      <Tooltip
+        arrow
+        title={
+          <>
+            fixed: {formatCurrency(variableSum)}
+            <br />
+            variable: {formatCurrency(fixedSum)}
+          </>
+        }
+      >
+        <Typography color="text.secondary">{formatCurrency(fixedSum + variableSum)}</Typography>
+      </Tooltip>
       <Typography color="text.secondary">= {formatCurrency(totalSum)}</Typography>
     </Box>
   );
